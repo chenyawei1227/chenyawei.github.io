5fd167e22539344bdc79bd7ff2fe6c3cac81c022
chenyawei1227 <cyw119900@gmail.com>
2020-04-24 20:50:21 +0800

    更新博客

--- js/toc.js
+++ js/toc.js
@@ -15,12 +15,3 @@ if (toc != null) {
 		 }
 	}
 }
-
-
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?2bfe7c1eb421b06edaabc733a86893df";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
